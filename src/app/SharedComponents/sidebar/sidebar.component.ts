43545929a76239fabae739d351f945a2edfd2d46
Cristina Robinson <NeuroBio@users.noreply.github.com>
2019-09-21 11:56:03 -0400

    show side bar entirely on screen

--- src/app/SharedComponents/sidebar/sidebar.component.ts
+++ src/app/SharedComponents/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, HostListener, AfterViewInit, AfterViewChecked, AfterContentInit, AfterContentChecked } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,15 +6,34 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 
-export class SideBarComponent {
+export class SideBarComponent implements AfterContentChecked{
+
 
   @Input() labels: string[] = ["default", "Sub"];
   @Input() linkList: string[][][] = [ [ ["tester 1", "tester1"], ["tester 2", "tester2"], ["tester 3", "tester3"] ],
                                     [ ['subtester 1', 'subtester1'] ] ];
   @Input() current: string = "tester2";
+  @ViewChild('container') container: ElementRef;
+  @ViewChild('list') list: ElementRef;
+ 
+  height = 0;
+
+  ngAfterContentChecked(){
+    this.onResize();
+  }
+  
+  @HostListener('window:scroll') 
+  onResize(){
+    if(this.height < this.list.nativeElement.offsetHeight){
+      this.height = window.innerHeight -
+      Math.max(this.container.nativeElement.getBoundingClientRect().top, 0)-3;
+      console.log("trigger")
+    }
+  }
 
   onSelect(selected: string) {
     this.current = selected;
   }
 
+ 
 }
