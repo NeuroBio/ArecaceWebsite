6cfaf6f23c296bc7cf02e08e5f4a6e60c61228a2
Cristina Robinson <NeuroBio@users.noreply.github.com>
2019-10-01 13:54:54 -0400

    set up survvey front end

--- src/app/playground/guild-survey/guild-survey.component.ts
+++ src/app/playground/guild-survey/guild-survey.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { SurveyQuestion } from './survey-question';
+import { SurveyQuestion, SurveyData, SurveyOutcome } from './survey-question';
+import { Observable } from 'rxjs';
+import { FireBaseService } from 'src/app/GlobalServices/firebase.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-guild-survey',
@@ -9,15 +12,33 @@ import { SurveyQuestion } from './survey-question';
 })
 export class GuildSurveyComponent implements OnInit {
 
-  questions: SurveyQuestion[] = [{Question: 'testing!', Answers: ['a', "b", 'c']},
-  {Question: 'Testing 2!', Answers: ['d', "e", 'f']}]
+  questions: SurveyQuestion[];// = [{Question: 'testing!', Answers: ['a', "b", 'c']},
+  //{Question: 'Testing 2!', Answers: ['d', "e", 'f']}]
+  outcomes: SurveyOutcome[];
+  results: object[][];
+  maxScores: object[];
   answers = this.fb.array([]);
   Form = this.createForm();
+  survey$: Observable<SurveyData>
   
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+              private firebaseserv: FireBaseService) { }
 
   ngOnInit() {
-    this.questions.forEach(() => this.addQuestion());
+    this.survey$ = this.firebaseserv.returnCollect('Surveys').pipe(map(x => {
+      return {Questions: JSON.parse(x[0].Questions),
+              Results: JSON.parse(x[0].Results),
+              Outcomes: JSON.parse(x[0].Outcomes),
+              MaxScores: JSON.parse(x[0].MaxScores)}
+    }));
+
+    this.survey$.subscribe(survey => {
+      this.questions = survey.Questions;
+      this.results = survey.Results;
+      this.outcomes = survey.Outcomes;
+      this.maxScores = survey.MaxScores;
+      survey.Questions.forEach(() => this.addQuestion());
+    })
   }
 
   createForm() {
@@ -32,7 +53,19 @@ export class GuildSurveyComponent implements OnInit {
   }
 
   onSubmit() {
-    const answers = this.Form.value.Answers.map(x => x.Answer)
-    console.log(answers)
+
+    //Calculate scores
+    let finalScores: object = {};
+    this.outcomes.forEach(o => {
+      finalScores[o.Name] = 0;
+    });
+
+    this.Form.value.Answers.forEach((q,i) =>{
+      let temp = this.results[i][q.Answer]
+      Object.keys(temp).forEach(key => finalScores[key] += +temp[key]);
+    });
+
+    console.log(finalScores);
+    
   }
 }
--- src/app/playground/guild-survey/survey-question.ts
+++ src/app/playground/guild-survey/survey-question.ts
@@ -7,3 +7,23 @@ export class SurveyQuestion {
         this.Answers = a;
     }
 }
+
+export class SurveyOutcome {
+    Name: string;
+    Outcomes: string;
+}
+
+export class SurveyData {
+    Questions: SurveyQuestion[];
+    Results: object[][];
+    Outcomes: SurveyOutcome[];
+    MaxScores: object[];
+
+    constructor(q: SurveyQuestion[], r: object[][], o: SurveyOutcome[],
+                m: object[]) {
+        this.Questions = q;
+        this.Results = r;
+        this.Outcomes = o;
+        this.MaxScores = m;
+    }
+}
