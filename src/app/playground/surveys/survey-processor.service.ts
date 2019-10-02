5473c688a663d6420960df5a52b0893aea61485e
Cristina Robinson <NeuroBio@users.noreply.github.com>
2019-10-02 15:08:14 -0400

    fix survey formarray scramble bug

--- src/app/playground/surveys/survey-processor.service.ts
+++ src/app/playground/surveys/survey-processor.service.ts
@@ -8,20 +8,28 @@ export class SurveyProcessorService {
 
   constructor() { }
 
-  standard(finalScores: Object, surveyData: SurveyData, answers: any[], keys: string[]) {
+  standard(finalScores: Object, surveyData: SurveyData, keys: string[]) {
     let finalOutcome = keys[0];
     keys.forEach(key => {
       if(finalScores[finalOutcome] < finalScores[key]){
         finalOutcome = key;
       }
     })
-
     const Outcome: SurveyOutcome = surveyData.Outcomes.find(o => o.Name === finalOutcome);
     const Match = Math.trunc(finalScores[finalOutcome]*1000)/10;
-    return({Outcome, Match});
+    const Name = surveyData.Name
+    return({Outcome: Outcome, Match: Match, Name: Name});
   }
 
   Guild(finalScores: Object, surveyData: SurveyData, answers: any[], keys: string[]) {
-    return this.standard(finalScores, surveyData, answers, keys);
+    let Results: any = {}
+    
+    Results = this.standard(finalScores, surveyData, keys);
+
+    if(answers[0] = 0) {
+     Results.Outcome.Text = "You are too young to apply for the Guilds right now.  However, if you were a little bit older, your results would be the following:"
+                            +  Results.Outcome.Text
+    }
+    return Results;
   }
 }
