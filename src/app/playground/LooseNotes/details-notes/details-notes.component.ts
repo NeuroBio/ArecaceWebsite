a5a76e6b62a63983593faed36f874e2fa79eebd8
Cristina Robinson <NeuroBio@users.noreply.github.com>
2020-03-05 18:13:21 -0500

    wire favorites for loosenotes

--- src/app/playground/LooseNotes/details-notes/details-notes.component.ts
+++ src/app/playground/LooseNotes/details-notes/details-notes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { LooseNotesMetaData } from 'src/app/Classes/ContentClasses';
-
+import { GetRouteSegmentsService } from 'src/app/GlobalServices/commonfunctions.service';
 @Component({
   selector: 'app-details-notes',
   templateUrl: './details-notes.component.html',
@@ -10,13 +10,20 @@ import { LooseNotesMetaData } from 'src/app/Classes/ContentClasses';
 export class DetailsNotesComponent implements OnInit {
 
   note: LooseNotesMetaData;
+  name: string;
+  path: string;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private getsegserv: GetRouteSegmentsService) { }
 
   ngOnInit() {
+    const mainPath = this.getsegserv.fetch(this.route.snapshot.pathFromRoot);
     this.route.data.subscribe((data: {Note: LooseNotesMetaData}) => {
       window.scroll(0,0);
-      this.note = data.Note});
+      this.note = data.Note;
+      this.name = this.note.ShortTitle;
+      this.path = `/${mainPath}/${this.note.ID}`
+    });
   }
 }
 
