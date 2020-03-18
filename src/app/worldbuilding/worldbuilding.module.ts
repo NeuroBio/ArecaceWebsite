6615a309dce126965a805211af0b6357f5c579df
Cristina Robinson <NeuroBio@users.noreply.github.com>
2020-03-18 15:16:01 -0400

    make image button component

--- src/app/worldbuilding/worldbuilding.module.ts
+++ src/app/worldbuilding/worldbuilding.module.ts
@@ -2,15 +2,16 @@ import { NgModule }                     from '@angular/core';
 import { CommonModule }                 from '@angular/common';
 
 import { WorldbuildingRoutingModule }   from './worldbuilding-routing.module';
-import { IntroductionComponent }        from './introduction/introduction.component';
-import { WorldbuildingComponent }       from './worldbuilding/worldbuilding.component';
-
 import { BestiaryModule }               from './bestiary/bestiary.module';
 import { CharactersModule }             from './characters/characters.module';
 import { GuildsModule }                 from './guilds/guilds.module';
 import { MapsModule }                   from './maps/maps.module';
 import { SourceSiphoidModule}           from './sourcesiphoid/sourcesiphoid.module';
 import { CultureModule }                from './culture/culture.module';
+import { ImageButtonModule }            from 'src/app/SharedComponentModules/SmallComponents/image-button/image-button.module';
+
+import { IntroductionComponent }        from './introduction/introduction.component';
+import { WorldbuildingComponent }       from './worldbuilding/worldbuilding.component';
 
 
 
@@ -28,6 +29,7 @@ import { CultureModule }                from './culture/culture.module';
     SourceSiphoidModule,
     CultureModule,
     MapsModule,
+    ImageButtonModule,
 
     WorldbuildingRoutingModule
   ]
