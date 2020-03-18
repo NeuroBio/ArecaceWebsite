e48abbc13f32f28cc25b326c9edf43b7bf75a12f
Cristina Robinson <NeuroBio@users.noreply.github.com>
2020-03-18 06:55:51 -0400

    add load spinner to downloads

--- src/app/SimplePages/downloadpage/download-page/download-page.component.ts
+++ src/app/SimplePages/downloadpage/download-page/download-page.component.ts
@@ -3,6 +3,7 @@ import { DownloadPageService } from '../download-page.service';
 import { ActivatedRoute } from '@angular/router';
 import { GetRouteSegmentsService } from 'src/app/GlobalServices/commonfunctions.service';
 import { Subscription } from 'rxjs';
+import { GlobalVarsService } from 'src/app/GlobalServices/global-vars.service';
 
 @Component({
   selector: 'app-download-page',
@@ -17,12 +18,15 @@ export class DownloadPageComponent implements OnInit, OnDestroy {
   path: string;
   stream: Subscription;
   full = false;
+  loading: boolean;
 
   constructor(private download: DownloadPageService,
               private route: ActivatedRoute,
-              private getsegserv: GetRouteSegmentsService) { }
+              private getsegserv: GetRouteSegmentsService,
+              private global: GlobalVarsService) { }
 
   ngOnInit() {
+    window.scroll(0,0);
     const mainPath = this.getsegserv.fetch(this.route.snapshot.pathFromRoot);
     this.path = mainPath.join('/');
     this.stream = this.download.ImageData
@@ -30,6 +34,7 @@ export class DownloadPageComponent implements OnInit, OnDestroy {
     this.name = this.ImageData.Name
       ? this.ImageData.Name
       : `${this.ImageData.FirstName} ${this.ImageData.LastName}`;
+    this.loading = this.global.ImagesLoadable.value;
   }
 
   ngOnDestroy() {
@@ -39,4 +44,5 @@ export class DownloadPageComponent implements OnInit, OnDestroy {
   switchView(){
     this.full = !this.full;
   }
+
 }
